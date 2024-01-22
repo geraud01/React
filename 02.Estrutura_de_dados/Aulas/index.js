b1877294beb7ddb84a856919194c15ef15a05518
Geraud Oliveira <geraud.pereira@hotmail.com>
2024-01-22 18:29:11 -0300

    Aula 2

---
+++ 02.Estrutura_de_dados/Aulas/index.js
@@ -0,0 +1,13 @@
+console.log(wuindow)
+
+document.getElementById("app");
+
+const string = 'test "teste"';
+const number = 2;
+const nullVar = null;
+const undefineVar = undefined;
+
+const vazio = []
+const strings = ['1', '2', '3']
+const numbers = [1, 2, 3,]
+const arrays = [[1, 2, 3,], [1, 2, 3,], [1, 2, 3,]]
