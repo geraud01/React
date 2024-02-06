d972126df3ed85a1b5e14398014698a9068d5a5f
Geraud Oliveira <geraud.pereira@hotmail.com>
2024-02-06 16:15:29 -0300

    lifecycle

--- m45/aulas/src/App.js
+++ m45/aulas/src/App.js
@@ -1,14 +1,19 @@
-
 import './App.css';
 import Header from './components/header';
 import CalculatorIMC from './components/IMC';
+import Form from './components/Form/Form'
+import Login from './components/Login/LoginControl'
 
 function App() {
   return (
     <div className="App">
-      <header title="Exercicio" />
-      <CalculatorIMC/> 
-      
+      {/* <Header title="Título do APP" />
+      <CalculatorIMC /> */}
+
+      {/* <Form title="props" /> */}
+
+      <Login name={'Geraud'} />
+
     </div>
   );
 }
